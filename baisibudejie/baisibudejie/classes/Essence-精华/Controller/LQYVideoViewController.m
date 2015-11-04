64965858aec74ca71fb60a7e19f09960f7535b13
shasha <1979474783@qq.com>
2015-11-04 22:38:58 +0800

    31-设置精华控制器的其他模块(视频,声音.图片,段子)

--- baisibudejie/baisibudejie/classes/Essence-精华/Controller/LQYVideoViewController.m
+++ baisibudejie/baisibudejie/classes/Essence-精华/Controller/LQYVideoViewController.m
@@ -3,50 +3,192 @@
 //  baisibudejie
 //
 //  Created by shasha on 15/10/28.
-//  Copyright © 2015年 shasha. All rights reserved.
+//  Copyright © 2015年 shasha. Video rights reserved.
 //
 
 #import "LQYVideoViewController.h"
+#import <MJExtension.h>
+#import "LQYHTTPSessionManager.h"
+#import "LQYTopic.h"
+#import "LQYTopic.h"
+#import "LQYTopicCell.h"
+#import "LQYDIYHeader.h"
+#import "LQYDIYFooter.h"
 
 @interface LQYVideoViewController ()
 
+@property (nonatomic, strong) LQYHTTPSessionManager *manager; /**< 请求管理者 */
+
+@property (nonatomic, strong) NSMutableArray <LQYTopic *> *topics; /**< 存放数组模型 */
+
+@property (nonatomic, copy) NSString *maxtime; /**< 用来存储加载下一页的参数 */
+
 @end
 
-@implementation LQYVideoViewController
+static NSString * const topicId = @"topicCell";
 
+@implementation LQYVideoViewController
+#pragma mark - 懒加载
+/** manager 属性的懒加载 */
+- (LQYHTTPSessionManager *)manager
+{
+    if (!_manager) {
+        _manager = [LQYHTTPSessionManager manager ];
+    }
+    return _manager;
+}
+
+#pragma mark - 初始化操作
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //设置 tableView
+    [self setupTableView];
+    
+    // 属性数据
+    [self refreshTopics];
+}
+
+/**
+ *  设置 tableView
+ */
+- (void)setupTableView
+{
     // 设置 tableView 的内边距 导航栏44 + 状态栏20 + 顶部标题40 = 104
     self.tableView.contentInset = UIEdgeInsetsMake(104, 0, 49, 0);
     // 设置滚动条的内边距
     self.tableView.scrollIndicatorInsets = self.tableView.contentInset;
+    self.tableView.backgroundColor = LQYCommonBgColor;
     
-     NSLogFun
+    //self.tableView.rowHeight = 500;
+    // 取消 cell 的分割线
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    // 注册
+    [self.tableView registerNib:[UINib nibWithNibName:NSStringFromClass([LQYTopicCell class]) bundle:nil] forCellReuseIdentifier:topicId];
 }
 
-#pragma mark - Table view data source
+/**
+ *  属性数据
+ */
+- (void)refreshTopics
+{
+    // 下拉刷新
+    self.tableView.header = [LQYDIYHeader headerWithRefreshingTarget:self refreshingAction:@selector(loadNewTopics)];
+    // 设置 header 的透明度
+    self.tableView.header.automaticallyChangeAlpha = YES;
+    // 进入刷新状态
+    [self.tableView.header beginRefreshing];
+    
+    // 上拉刷新
+    self.tableView.footer = [LQYDIYFooter footerWithRefreshingTarget:self refreshingAction:@selector(loadMoreTopics)];
+}
+#pragma mark - 刷新数据
+/**
+ *  上拉刷新加载更多数据
+ */
+- (void)loadMoreTopics
+{
+    // 请求参数
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    parameters[@"a"] = @"list";
+    parameters[@"type"] = @41;
+    parameters[@"c"] = @"data";
+    parameters[@"maxtime"] = self.maxtime;
+    
+    __weak typeof(self) wealSelf = self;
+    [self.manager GET:LQYRequestUrl parameters:parameters success:^(NSURLSessionDataTask * _Nonnull task, id  _Nonnull responseObject) {
+        
+        // 存储返回下一页的数据
+        wealSelf.maxtime = responseObject[@"info"][@"maxtime"];
+        
+        // 字典转模型
+        NSArray *moreTopics = [LQYTopic objectArrayWithKeyValuesArray:responseObject[@"list"]];
+        
+        // 拼接数组 ,把新返回的数据拼接的帖子数组中
+        [wealSelf.topics addObjectsFromArray:moreTopics];
+        
+        // 刷新数据
+        [wealSelf.tableView reloadData];
+        
+        // 停止刷新
+        [wealSelf.tableView.footer endRefreshing];
+        
+    } failure:^(NSURLSessionDataTask * _Nonnull task, NSError * _Nonnull error) {
+        if (error) {
+            LQYLog(@"%@",error);
+        }
+        // 停止刷新
+        [wealSelf.tableView.footer endRefreshing];
+        
+    }];
+}
+
+/**
+ *  下拉刷新加载数据
+ */
+- (void)loadNewTopics
+{
+    // 请求参数
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    parameters[@"a"] = @"list";
+    parameters[@"c"] = @"data";
+    parameters[@"type"] = @41;
+    
+    __weak typeof(self) weakSelf = self;
+    
+    [self.manager GET:LQYRequestUrl parameters:parameters success:^(NSURLSessionDataTask * _Nonnull task, id  _Nonnull responseObject) {
+        
+        // 存储返回下一页的数据
+        weakSelf.maxtime = responseObject[@"info"][@"maxtime"];
+        
+        // 字典转模型
+        weakSelf.topics = [LQYTopic objectArrayWithKeyValuesArray:responseObject[@"list"]];
+        
+        // 刷新数据
+        [weakSelf.tableView reloadData];
+        
+        // 结束刷新
+        [weakSelf.tableView.header endRefreshing];
+        
+    } failure:^(NSURLSessionDataTask * _Nonnull task, NSError * _Nonnull error) {
+        if (error) {
+            LQYLog(@"%@",error);
+        }
+        // 结束刷新
+        [weakSelf.tableView.header endRefreshing];
+        
+    }];
+    
+}
+
+#pragma mark - tableView的数据源方法
 /**
  * 返回一共有多少行
  */
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
-    return 50;
+    return self.topics.count;
 }
 
 /**
  *  返回 cell
  */
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" ];
+    LQYTopicCell *cell = [tableView dequeueReusableCellWithIdentifier:topicId];
     
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
-    }
+    LQYTopic *topic = self.topics[indexPath.row];
     
-    cell.textLabel.text = [NSString stringWithFormat:@"%@ - 第 %zd 行",self.title,indexPath.row];
+    // 设置数据
+    cell.topic = topic;
     
     return cell;
+}
+
+#pragma mark - tableView 代理方法
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    return self.topics[indexPath.row].cellHeight;
 }
 
 @end
